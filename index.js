94b3537edb8f88c6be77c2388793b32f7a3d7227
andresauterio <andresauterio@gmail.com>
2022-08-26 22:01:54 -0300

    Mejora con objetos colecciones y for

--- index.js
+++ index.js
@@ -1,79 +1,76 @@
 
 const redonda = 1;
-const blanca = (redonda/2);
-const negra = (redonda/4);
-const corchea = (negra/2);
-const semicorchea = (negra/4);
-const fusa = (negra/8);
-const semifusa = (negra/16);
+const blanca = (redonda / 2);
+const negra = (redonda / 4);
+const corchea = (negra / 2);
+const semicorchea = (negra / 4);
+const fusa = (negra / 8);
+const semifusa = (negra / 16);
+
+var respuestas = {
+    correctas: 0,
+    incorrectas: 0,
+}
+
+let preguntas = [
+    {
+        "pregunta": "cuantas corcheas entran en una redonda(1 tiempo)",
+        "respuesta": 8,
+    },
+    {
+        "pregunta": "Que valor tiene una semifusa con respecto a una redonda?",
+        "respuesta": 64,
+    },
+    {
+        "pregunta": "Que valor tienen 2 Redondas?",
+        "respuesta": 2,
+    }
+]
+
+function sonido() {
+    alert("Una Redonda sonaría TA" + " " + redonda + " " + "Tiempo");
+    // alert("Una Blanca sonaría TA TA" + " " + blanca + " " + "1/2 Tiempos");
+    // alert("Una negra sonaría TA TA TA TA" + " " + negra + " " + "1/4 Tiempos");
+    // alert("Una Corchea sonaria TA TA TA TA TA TA TA TA" + " " + corchea + " " + " 1/8 Ocho Tiempos");
+    // alert("Una semicorchea sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA" + " " + semicorchea + " " + "1/16 tiempos");
+    // alert("Una Fusa sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA" + " " + fusa + " " + "1/32 tiempos");
+    // alert("Una Semifusa sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA" + " " + semifusa + " " + "1/64 tiempos");
+}
+
+function hacerPregunta() {
+    var respuesta = "";
+
+    console.log(preguntas.length);
+    var i = 0;
 
 
-function sonido (){
-   alert("Una Redonda sonaría TA"+" "+ redonda +" "+ "Tiempo" );
-    alert( "Una Blanca sonaría TA TA" +" " + blanca + " "+"1/2 Tiempos");
-    alert("Una negra sonaría TA TA TA TA"+ " " + negra+ " "+"1/4 Tiempos");
-    alert("Una Corchea sonaria TA TA TA TA TA TA TA TA"+" " +corchea+ " "+ " 1/8 Ocho Tiempos");
-    alert("Una semicorchea sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA"+" " +semicorchea+ " " +"1/16 tiempos");
-    alert("Una Fusa sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA"+" "+fusa +" "+ "1/32 tiempos");
-    alert("Una Semifusa sonaria TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA TA"+" "+semifusa+" "+ "1/64 tiempos");
+    for (i = 0; i <= (preguntas.length - 1); i++) {
+
+        respuesta = prompt(preguntas[i].pregunta);
+        if (respuesta == preguntas[i].respuesta) {
+            alert("Bien ahi");
+            respuestas.correctas++;
+        }
+        else {
+            respuestas.incorrectas++;
+        }
+    }
+    return respuestas;
 }
 
 sonido();
-function question(){
-const pregunta = Number(prompt("cuantas corcheas entran en una redonda (1 tiempo)"));
-if (pregunta==8){
-    alert("Perfecto")
-    question2();
-}
-else if(pregunta!==8){
-const respuesta = prompt("Te equivocaste. Quieres repasar el contenido?");
-}else if(respuesta==='si'){
-    sonido();}
-}
-question();
-function question2(){
-const pregunta2 = Number(prompt("Que valor tiene una semifusa con respecto a una redonda?"));
-if (pregunta2==64){
-    alert("Perfecto")
-    question3();
-}
-else if(pregunta2!==64){
-    const respuesta = prompt("Te equivocaste. Quieres repasar el contenido?");
-    }else if(respuesta=="si")
+var rtas = hacerPregunta();
+
+alert("Contestaste " + rtas.correctas + " bien y " + rtas.incorrectas + " mal")
+
+if (rtas.incorrectas > 0) {
+    if (prompt("Te falta practica, queres ver de nuevo la teoria?") == "si") {
         sonido();
-}
-function question3(){
-    const pregunta3 = Number(prompt("Que valor tienen 2 Redondas?"));
-    if (pregunta3==2){
-        alert("Perfecto")
-        question4();
     }
-    else if(pregunta3!==2){
-        const respuesta = prompt("Te equivocaste. Quieres repasar el contenido?");
-        }else if(respuesta=="si"){
-            sonido();}
-    else (respuesta!="si") 
-    alert("Hasta Luego");
-        }
-
-
-// }
-// function direccion(calle, numeracion){
-//     const direccion = prompt("Ingrese su calle y numeración");
-// }
-// function edades(){
-//     const edad = Number(prompt("Ingrese su edad"));
-
-// if (edad>= 18) {
-//     alert("eres mayor de edad");
-// }else{
-//     alert("todavia eres menor")
-// }
-    
-
-// }
-
-
-// nombre();
-// direccion();
-// edades();
+    else {
+        alert("jeropaa");
+    }
+}
+else {
+    alert("Puntuacion perfecta loquillo!!");
+}
